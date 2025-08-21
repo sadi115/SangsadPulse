7068bf9a3c51beb55a04d0c8456e291b662bd91b
Sadi Anwar Chowdhury <sadi115@gmail.com>
2025-08-21 19:19:32 +0000

    Make the whole website colorful.
    and theme transition not working perf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            disableTransitionOnChange
         >
             {children}
             <Toaster />
